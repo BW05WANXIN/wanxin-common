ae22ef397728aca0f05a4aad2f73bd4ad63f58f2
BW05WANXIN <2293152624@qq.com>
2019-09-09 19:12:37 +0800

    首次测试，添加Junit工具

--- src/test/java/com/wanxin/common/testUtils/TestStringUtil.java
+++ src/test/java/com/wanxin/common/testUtils/TestStringUtil.java
@@ -3,10 +3,20 @@
  */
 package com.wanxin.common.testUtils;
 
+import org.junit.Test;
+
+import com.wanxin.common.utils.StringUtil;
+
 /**
  * @author 926474
  *
  */
 public class TestStringUtil {
+	
+	@Test
+	public void testHasText() {
+		boolean hasText = StringUtil.hasText(" 是女的房价款和");
+		System.out.println("是否有值："+hasText);
+	}
 
 }
